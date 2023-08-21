9d13c3642e3ee12c908bd2dffba16d0b140da6b7
Nguyen Van Duc <duccdt4@gmail.com>
2023-08-22 00:55:05 +0700

    Xây dựng UI phần Header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+function AccountsItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                src="https://p9-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/751d9281c7f18830a694812b0643f720.jpeg?x-expires=1692716400&x-signature=NeECzxdwaMIR9gKoLSVHj8kqtYc%3D"
+                alt="Hoaa"
+                className={cx('avatar')}
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}></FontAwesomeIcon>
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountsItem;
